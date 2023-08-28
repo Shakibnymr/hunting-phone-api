bd43670ece0bcf92467fc408bce3f14095588a27
shakibnymr <shakibnymr11@gmail.com>
2023-08-28 14:50:56 +0600

    toggling

--- js/phone.js
+++ js/phone.js
@@ -46,14 +46,28 @@ const displayPhones = (phones) => {
     // 4. append child
     phoneContainer.appendChild(phoneCard);
   });
+
+  //hide loading spinner
+  toggleLoadingSpinner(false);
 };
 
 // handle search button
 const handleSearch = () => {
+  toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   console.log(searchText);
   loadPhone(searchText);
 };
+
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById('spinner');
+  if(isLoading){
+    loadingSpinner.classList.remove('hidden');
+  }
+  else {
+    loadingSpinner.classList.add('hidden');
+  }
+}
 
 // loadPhone();
