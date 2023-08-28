6f876964df9407dc3ebf74bdf42d507b300cf2eb
shakibnymr <shakibnymr11@gmail.com>
2023-08-28 15:57:50 +0600

    show all handled

--- js/phone.js
+++ js/phone.js
@@ -1,31 +1,34 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
   //  console.log(phones);
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, isShowAll) => {
   // console.log(phones)
 
   const phoneContainer = document.getElementById("phone-container");
   //clear phone container cards before adding new cards
   phoneContainer.textContent = "";
 
-  //display showall button if there are more than 12 phones
+  //display show all button if there are more than 12 phones
   const showAllContainer = document.getElementById("show-all-container");
-  if (phones.length > 12) {
+  if (phones.length > 12 && !isShowAll) {
     showAllContainer.classList.remove("hidden");
-  }
-  else{
-    showAllContainer.classList.add('hidden')
+  } else {
+    showAllContainer.classList.add("hidden");
   }
 
-  //display only first 12 phones
-  phones = phones.slice(0, 12);
+  console.log("is show all", isShowAll);
+
+  //display only first 12 phones if not show all
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
     console.log(phone);
@@ -52,22 +55,28 @@ const displayPhones = (phones) => {
 };
 
 // handle search button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   console.log(searchText);
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 };
 
+//spin controlling
 const toggleLoadingSpinner = (isLoading) => {
-  const loadingSpinner = document.getElementById('spinner');
-  if(isLoading){
-    loadingSpinner.classList.remove('hidden');
+  const loadingSpinner = document.getElementById("spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
   }
-  else {
-    loadingSpinner.classList.add('hidden');
-  }
-}
+};
+
+//handle show all
+
+const handleShowAll = () => {
+  handleSearch(true);
+};
 
 // loadPhone();
