8842ba3d3a0799e56c405e78b8d4033197d17fc7
shakibnymr <shakibnymr11@gmail.com>
2023-08-28 22:14:32 +0600

    adding modal

--- js/phone.js
+++ js/phone.js
@@ -42,7 +42,7 @@ const displayPhones = (phones, isShowAll) => {
       <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
       <div class="card-actions justify-center">
-        <button onclick="handleShowDetails()" class="btn btn-primary">Show Details</button>
+        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `;
@@ -55,12 +55,16 @@ const displayPhones = (phones, isShowAll) => {
 
 
 
-
-const handleShowDetails = () => {
-  console.log('clicked')
+//control show details button
+const handleShowDetails = async (id) => {
+  console.log('clicked', id)
+  //load single phone data
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  console.log(data);
 }
 
-//5555555555 : upore ja
+
 
 
 // handle search button
